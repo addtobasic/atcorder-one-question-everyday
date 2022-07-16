63d7da81794b4c8411174d0101424ae1e29a0f39
addtobasic <addtobasic@gmail.com>
2022-07-17 00:07:26 +0900

    :sparkles: ABC081Aを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,17 +9,12 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-        3 4
+            101
 "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(
-        output.stdout_str(),
-        "
-    Even
-    \n"
-    );
+    assert_eq!(output.stdout_str(), "2\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -30,16 +25,11 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-        1 21
+          000
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(
-        output.stdout_str(),
-        "
-    Odd
-    \n"
-    );
+    assert_eq!(output.stdout_str(), "0\n");
     assert!(output.stderr_str().is_empty());
 }
