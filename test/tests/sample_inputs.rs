4fc554722daf844f69d4ad1077c492dcaf6e9dbd
addtobasic <addtobasic@gmail.com>
2022-07-31 15:49:23 +0900

    :sparkles: ABC175Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,16 +9,13 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            4 5
-            0 5
-            -2 4
-            3 4
-            4 -4
+            5
+            4 4 9 7 5
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "3\n");
+    assert_eq!(output.stdout_str(), "5\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -29,24 +26,13 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            12 3
-            1 1
-            1 1
-            1 1
-            1 1
-            1 2
-            1 3
-            2 1
-            2 2
-            2 3
-            3 1
-            3 2
-            3 3
+            6
+            4 5 4 3 3 5
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "7\n");
+    assert_eq!(output.stdout_str(), "8\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -57,31 +43,29 @@ fn sample3() {
         .cmd()
         .output_with_stdin(
             r#"
-            20 100000
-            14309 -32939
-            -56855 100340
-            151364 25430
-            103789 -113141
-            147404 -136977
-            -37006 -30929
-            188810 -49557
-            13419 70401
-            -88280 165170
-            -196399 137941
-            -176527 -61904
-            46659 115261
-            -153551 114185
-            98784 -6820
-            94111 -86268
-            -30401 61477
-            -55056 7872
-            5901 -163796
-            138819 -185986
-            -69848 -96669
+            10
+            9 4 6 1 9 6 10 6 6 8
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "6\n");
+    assert_eq!(output.stdout_str(), "39\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"
+            2
+            1 1
+        "#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "0\n");
     assert!(output.stderr_str().is_empty());
 }
