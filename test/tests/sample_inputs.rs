4983243c735231173b41c924e63565e985eb283e
addtobasic <addtobasic@gmail.com>
2022-07-31 19:41:46 +0900

    :sparkles: ABC176Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,13 +9,12 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            5
-            4 4 9 7 5
+            123456789
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "5\n");
+    assert_eq!(output.stdout_str(), "Yes\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -26,13 +25,12 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            6
-            4 5 4 3 3 5
+            0
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "8\n");
+    assert_eq!(output.stdout_str(), "Yes\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -43,29 +41,11 @@ fn sample3() {
         .cmd()
         .output_with_stdin(
             r#"
-            10
-            9 4 6 1 9 6 10 6 6 8
+            31415926535897932384626433832795028841971693993751058209749445923078164062862089986280
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "39\n");
-    assert!(output.stderr_str().is_empty());
-}
-
-#[test]
-fn sample4() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(
-            r#"
-            2
-            1 1
-        "#,
-        )
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "0\n");
+    assert_eq!(output.stdout_str(), "No\n");
     assert!(output.stderr_str().is_empty());
 }
