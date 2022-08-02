e35e458c56bef0c3d0f3c6dd637556388fb9efb5
addtobasic <addtobasic@gmail.com>
2022-08-02 21:50:06 +0900

    :sparkles: ABC177Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,18 +9,13 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            5 6
-            1 5
-            4 5
-            2 3
-            1 4
-            3 5
-            2 5
+            cabacc
+            abc
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "2\n");
+    assert_eq!(output.stdout_str(), "1\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -31,38 +26,12 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            3 1
-            1 2
+            codeforces
+            atcoder
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "0\n");
-    assert!(output.stderr_str().is_empty());
-}
-
-#[test]
-fn sample3() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(
-            r#"
-            7 10
-            1 7
-            5 7
-            2 5
-            3 6
-            4 7
-            1 5
-            2 4
-            1 3
-            1 6
-            2 7
-        "#,
-        )
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "4\n");
+    assert_eq!(output.stdout_str(), "6\n");
     assert!(output.stderr_str().is_empty());
 }
