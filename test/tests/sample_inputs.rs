cd2500ed2d528b7da69c1392820565a49ee0e928
addtobasic <addtobasic@gmail.com>
2022-08-03 22:16:11 +0900

    :sparkles: ABC178Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,13 +9,12 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            cabacc
-            abc
+            1 2 1 1
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "1\n");
+    assert_eq!(output.stdout_str(), "2\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -26,12 +25,27 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            codeforces
-            atcoder
+            3 5 -4 -2
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "6\n");
+    assert_eq!(output.stdout_str(), "-6\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"
+            -1000000000 0 -1000000000 0
+        "#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "1000000000000000000\n");
     assert!(output.stderr_str().is_empty());
 }
