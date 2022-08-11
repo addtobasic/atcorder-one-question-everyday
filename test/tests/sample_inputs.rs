6a0f32ffc943abf759a96866b6be051e26815de0
addtobasic <addtobasic@gmail.com>
2022-08-11 22:55:32 +0900

    :sparkles: ABC185Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,13 +9,14 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            3 0
-            xox
+            10 2 20
+            9 11
+            13 17
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "0\n");
+    assert_eq!(output.stdout_str(), "Yes\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -26,13 +27,14 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            20 199999
-            oooooooooxoooooooooo
+            10 2 20
+            9 11
+            13 16
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "200017\n");
+    assert_eq!(output.stdout_str(), "No\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -43,12 +45,48 @@ fn sample3() {
         .cmd()
         .output_with_stdin(
             r#"
-            20 10
-            xxxxxxxxxxxxxxxxxxxx
+            15 3 30
+            5 8
+            15 17
+            24 27
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "0\n");
+    assert_eq!(output.stdout_str(), "Yes\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"
+            20 1 30
+            20 29
+        "#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "No\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample5() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"
+            20 1 30
+            1 10
+        "#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "No\n");
     assert!(output.stderr_str().is_empty());
 }
