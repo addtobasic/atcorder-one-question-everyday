3ee66c5feb7ad976ff7af1b0e66d6e21ccb559ed
addtobasic <addtobasic@gmail.com>
2022-08-16 13:17:04 +0900

    :sparkles: ABC191Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -9,16 +9,13 @@ fn sample1() {
         .cmd()
         .output_with_stdin(
             r#"
-            4 9 9
             5 5
-            15 5
-            5 15
-            15 15
+            3 5 6 5 4
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "Yes\n");
+    assert_eq!(output.stdout_str(), "3 6 4\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -29,37 +26,12 @@ fn sample2() {
         .cmd()
         .output_with_stdin(
             r#"
-            3 691 273
-            691 997
-            593 273
-            691 273
+            3 3
+            3 3 3
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
-    assert!(output.stderr_str().is_empty());
-}
-
-#[test]
-fn sample3() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(
-            r#"
-            7 100 100
-            10 11
-            12 67
-            192 79
-            154 197
-            142 158
-            20 25
-            17 108
-        "#,
-        )
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "Yes\n");
+    assert_eq!(output.stdout_str(), "\n");
     assert!(output.stderr_str().is_empty());
 }
