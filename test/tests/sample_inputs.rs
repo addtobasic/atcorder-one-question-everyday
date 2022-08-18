653b667ea5033103b8134ba7f81d1791098a75db
addtobasic <addtobasic@gmail.com>
2022-08-18 13:00:19 +0900

    :sparkles: ABC194Bを解いた

--- test/tests/sample_inputs.rs
+++ test/tests/sample_inputs.rs
@@ -10,14 +10,14 @@ fn sample1() {
         .output_with_stdin(
             r#"
             3
-            3 9 5
-            4 8 5
-            5 7 5
+            8 5
+            4 4
+            7 9
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "8\n");
+    assert_eq!(output.stdout_str(), "5\n");
     assert!(output.stderr_str().is_empty());
 }
 
@@ -29,39 +29,13 @@ fn sample2() {
         .output_with_stdin(
             r#"
             3
-            5 9 5
-            6 8 5
-            7 7 5
+            11 7
+            3 2
+            6 7
         "#,
         )
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "-1\n");
-    assert!(output.stderr_str().is_empty());
-}
-
-#[test]
-fn sample3() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(
-            r#"
-            10
-            158260522 877914575 602436426
-            24979445 861648772 623690081
-            433933447 476190629 262703497
-            211047202 971407775 628894325
-            731963982 822804784 450968417
-            430302156 982631932 161735902
-            880895728 923078537 707723857
-            189330739 910286918 802329211
-            404539679 303238506 317063340
-            492686568 773361868 125660016
-        "#,
-        )
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "861648772\n");
+    assert_eq!(output.stdout_str(), "5\n");
     assert!(output.stderr_str().is_empty());
 }
