ebece20b129d22edacc516dbe994cbc6e730c6a5
addtobasic <addtobasic@gmail.com>
2022-08-22 12:04:55 +0900

    :sparkles: ABC200Bを解いた

---
+++ A/ABC200B.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+use proconio::marker::Chars;
+use proconio::marker::Usize1;
+use std::cmp;
+use std::num;
+
+fn main() {
+    input! {
+        mut n: usize,
+        k: usize,
+    }
+
+    for _ in 0..k {
+        if n % 200 == 0 {
+            n /= 200;
+        } else {
+            n = n * 1000 + 200;
+        }
+    }
+
+    println!("{}", n);
+}
